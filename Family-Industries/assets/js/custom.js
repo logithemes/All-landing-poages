1f037c8a6820ade341ee82a4fc829c5ff64afc77
unknown <logeshmyown1@gmail.com>
2023-10-04 08:43:14 +0530

    done1

--- Family-Industries/assets/js/custom.js
+++ Family-Industries/assets/js/custom.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         $(".menu").removeClass("menuact");
     })
 
+    
 
     //BANNER ANIMATION
     $(".banner .ban-lhs").addClass("ani1");
@@ -78,78 +79,7 @@ $(window).on("scroll", function(){
 
 });
 
-   // Gallery image hover
-   $(".img-wrapper").hover(
-    function () {
-        $(this).find(".img-overlay").animate({
-            opacity: 1
-        }, 600);
-    },
-    function () {
-        $(this).find(".img-overlay").animate({
-            opacity: 0
-        }, 600);
-    }
-);
-
-// Lightbox
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $prevButton = $('<div id="prevButton"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></div>');
-var $nextButton = $('<div id="nextButton"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></div>');
-var $exitButton = $('<div id="exitButton"> <i class="fa fa-times" aria-hidden="true"></i></div>');
-
-// Add overlay
-$overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
-$("#gallery").append($overlay);
-
-// Hide overlay on default
-$overlay.hide();
-
-// When an image is clicked
-$(".img-overlay").click(function (event) {
-    event.preventDefault();
-    var imageLocation = $(this).prev().attr("href");
-    $image.attr("src", imageLocation);
-    $overlay.fadeIn("slow");
-});
-
-// When the overlay is clicked
-$overlay.click(function () {
-    $(this).fadeOut("slow");
-});
-
-// When next button is clicked
-$nextButton.click(function (event) {
-    $("#overlay img").hide();
-    var $currentImgSrc = $("#overlay img").attr("src");
-    var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-    var $nextImg = $($currentImg.closest(".plac-gal-imag").next().find("img"));
-    var $images = $("#image-gallery img");
-    if ($nextImg.length > 0) {
-        $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-    } else {
-        $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
-    }
-    event.stopPropagation();
-});
-
-// When previous button is clicked
-$prevButton.click(function (event) {
-    $("#overlay img").hide();
-    var $currentImgSrc = $("#overlay img").attr("src");
-    var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-    var $nextImg = $($currentImg.closest(".plac-gal-imag").prev().find("img"));
-    $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-    event.stopPropagation();
-});
-
-$exitButton.click(function () {
-    $("#overlay").fadeOut("slow");
-});
   
-
-
 
 });
 
